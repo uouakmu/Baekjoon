beb30e4a514d30056a9efde0dbdc0f5c200bf3c5
혀기찬 <chan5759@naver.com>
2024-08-04 14:56:16 +0900

    [Silver IV] Title: 스택 2, Time: 880 ms, Memory: 71112 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(sys.stdin.readline())
+ST = []
+for i in range(N):
+    op = sys.stdin.readline().split()
+    if op[0] == '1':
+        ST.append(int(op[-1]))
+    elif op[0] == '2':
+        if ST:
+            print(ST.pop(-1))
+            continue
+        print(-1)
+    elif op[0] == '3':
+        print(len(ST))
+    elif op[0] == '4':
+        if ST:
+            print(0)
+            continue
+        print(1)
+    elif op[0] == '5':
+        if ST:
+            print(ST[-1])
+            continue
+        print(-1)
