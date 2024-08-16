6df254cd004b243727ba6489743b7ff85c9e7b80
혀기찬 <chan5759@naver.com>
2024-08-16 09:52:02 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,12 @@
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for i in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
+print(fibo(int(input())))
