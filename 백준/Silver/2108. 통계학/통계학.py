1714e48589d434ed164ab06bc353b89d29d56f0e
혀기찬 <chan5759@naver.com>
2024-09-15 23:03:14 +0900

    [Silver III] Title: 통계학, Time: 432 ms, Memory: 52796 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = []
+
+sum = 0
+count = dict()
+for _ in range(n):
+    x = int(input())
+    data.append(x)
+
+    sum += x
+
+    if x not in count:
+        count[x] = 1
+    else:
+        count[x] += 1
+
+data.sort()
+print(round(sum/n))
+print(data[n//2])
+
+freq = max(count.values())
+numbers = []
+for key, value in count.items():
+    if value == freq:
+        numbers.append(key)
+
+if len(numbers) == 1:
+    print(numbers[0])
+else:
+    print(sorted(numbers)[1])
+
+print(data[-1] - data[0])
