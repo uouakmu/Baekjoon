b5f1412e931bf007a4323831c159a46399896037
혀기찬 <chan5759@naver.com>
2024-12-11 10:56:18 +0900

    [Bronze V] Title: X보다 작은 수, Time: 940 ms, Memory: 50672 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			int k = sc.nextInt();
+			if (k<x) {
+				System.out.printf("%d ",k);
+			}
+		}
+	}
+}
