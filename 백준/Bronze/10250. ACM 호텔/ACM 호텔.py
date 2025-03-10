ae6a7709f346aff1464eff66aed1ee1105825bb0
혀기찬 <chan5759@naver.com>
2025-03-10 11:08:26 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,16 +1,11 @@
-for _ in range(int(input())):
-    h,w,n = map(int,input().split())
-
-    hotel = [[0]*w for _ in range(h)]
-    guest = 0
-
-    for j in range(w):
-        for i in range(h):
-            if hotel[i][j]==0:
-                guest+=1
-                hotel[i][j]=1
-                if guest == n:
-                    print(f"{i+1}{j+1:02d}")
-                    break
-        if guest == n:
-            break
+num = int(input())
+ 
+for i in range(num):
+    H, W, N = map(int, input().split())
+    floor = N % H
+    room = N // H + 1
+    if floor == 0:
+        room -= 1
+        floor = H
+        
+    print(floor*100 + room)
