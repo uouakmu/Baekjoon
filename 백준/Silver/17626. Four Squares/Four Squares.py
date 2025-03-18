3fd18bd2d7e9105e975b4321ce9c9704bc47a742
혀기찬 <chan5759@naver.com>
2025-03-18 13:58:24 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 110060 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,20 @@
+n = int(input())
+dp = [0] * (n+1)
+
+k = 1
+while k**2<=n:
+    dp[k**2] = 1
+    k+=1
+
+for i in range(1, n+1):
+    if dp[i] != 0:
+        continue
+    
+    j = 1
+    while j*j <= i:
+        if dp[i] == 0:
+            dp[i] = dp[j*j] + dp[i-j*j]
+        else:
+            dp[i] = min(dp[i], dp[j*j] + dp[i-j*j])
+        j += 1
+print(dp[n])
