cc8d1aa4b392c4ed5fb1fe56af69ed9f08527dd6
혀기찬 <chan5759@naver.com>
2025-04-12 23:57:01 +0900

    [Silver I] Title: IOIOI, Time: 324 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,17 +1,34 @@
-# 5525
-
 import sys
-input = sys.stdin.readline
 
-n = int(input())
-slen = int(input())
-s = input()
+N = int(sys.stdin.readline().strip())
+M = int(sys.stdin.readline().strip())
+S = sys.stdin.readline().strip()
 
-pn = 'I'+'OI'*n
+P = 'IOI'
 
-cnt = 0
-for i in range(slen):
-  if s[i] == 'I':
-    if s[i:i+(n*2+1)] == pn and i <= slen:
-      cnt += 1
-print(cnt)
+cnt = i = answer = 0
+
+# 입력받은 S의 길이만큼 반복
+while i < (M - 1):
+  # 현재 반복되는 문자열이 'IOI'냐 ?
+  if S[i : i+3] == P:
+    # 그렇다면 다음에도 반복하는지 확인하기 위해 i+2
+    i += 2
+    # 'IOI' 반복 수 저장
+    cnt += 1
+    # 반복 수 cnt가 우리가 원하는 N과 동일하냐 ?
+    if cnt == N:
+      # 그렇다면 Pn을 찾은 것이므로 answer + 1
+      answer += 1
+      # 지금 Pn의 일부를 포함해서 또다른 Pn이 나올 수 있으므로 
+      # cnt를 초기화하지 않고 -1만 함
+      cnt -= 1
+
+  # 현재 반복되는 문자열이 'IOI'가 아니냐 ?
+  else:
+    # 그럼 다음 인덱스로 이동
+    i += 1
+    # cnt 초기화
+    cnt = 0
+
+print(answer)
