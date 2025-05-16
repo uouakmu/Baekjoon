1dbef6ed50f896f90ae30b829a32c5693f3223f5
혀기찬 <chan5759@naver.com>
2025-05-16 10:01:37 +0900

    [Bronze IV] Title: 탁구 경기, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27918. 탁구 경기/탁구 경기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+d = 0
+p = 0
+for _ in range(n):
+    s = input().rstrip()
+    if s == 'D':
+        d += 1
+    else:
+        p += 1
+
+    if abs(d-p) > 1:
+        print(f"{d}:{p}")
+        break
+else:
+    print(f"{d}:{p}")
