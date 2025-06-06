25e3de829a09741ecb1c9d1493a4b43f770e2d07
혀기찬 <chan5759@naver.com>
2025-06-06 10:08:49 +0900

    [Silver V] Title: 줄 세우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11536. 줄 세우기/줄 세우기.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = [] 
+for _ in range(n):
+    name = input()
+    arr.append(name)
+
+if sorted(arr) == arr:
+        print("INCREASING")
+elif sorted(arr, reverse=True) == arr:
+        print("DECREASING")
+else:
+        print("NEITHER")
