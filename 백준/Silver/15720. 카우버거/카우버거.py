3791f0bb40e286511aee61a0218b2372879ce6e0
혀기찬 <chan5759@naver.com>
2025-09-12 13:56:12 +0900

    [Silver V] Title: 카우버거, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15720. 카우버거/카우버거.py
@@ -0,0 +1,15 @@
+b, c, d = map(int, input().split())
+burger = list(map(int, input().split()))
+side = list(map(int, input().split()))
+beverage = list(map(int, input().split()))
+
+burger.sort(reverse=True)
+side.sort(reverse=True)
+beverage.sort(reverse=True)
+
+total = sum(burger) + sum(side) + sum(beverage)
+print(total)
+for i in range(min(len(burger), len(side), len(beverage))):
+    temp = (burger[i] + side[i] + beverage[i])
+    total -= temp // 10
+print(total)
