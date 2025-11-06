5254efb41fa809bb1eddbaf04d37776bc01e6fb1
혀기찬 <chan5759@naver.com>
2025-11-06 14:03:35 +0900

    [Bronze III] Title: Viva la Diferencia, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4084. Viva la Diferencia/Viva la Diferencia.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b, c, d = map(int, input().split())
+    if a == b == c == d == 0:
+        break
+
+    res = 0
+    while not a == b == c == d:
+        a, b, c, d = abs(a-b), abs(b-c), abs(c-d), abs(d-a)
+        res += 1
+    print(res)
