1d500a39bac9a18543d9a1b6bfc8ef94a9a0b97b
hyeogi <chan5759@naver.com>
2025-12-04 19:27:56 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 172 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if (a == b) {
+            System.out.println("Anything");
+        } else if (a > b) {
+            System.out.println("Subway");
+        } else {
+            System.out.println("Bus");
+        }
+    }
+}
