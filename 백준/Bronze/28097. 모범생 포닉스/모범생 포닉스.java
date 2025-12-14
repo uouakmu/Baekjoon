dcc46649464285b00688fc5352da80324649be48
hyeogi <chan5759@naver.com>
2025-12-14 11:52:44 +0900

    [Bronze IV] Title: 모범생 포닉스, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/28097. 모범생 포닉스/모범생 포닉스.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int sumTime = (n - 1) * 8;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			sumTime += Integer.parseInt(st.nextToken());
+		}
+		sb.append(sumTime / 24).append(" ").append(sumTime % 24);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
