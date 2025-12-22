ee8f1c90793a328abe70364d76ae8aedd14e8eba
hyeogi <chan5759@naver.com>
2025-12-22 22:16:41 +0900

    [Bronze IV] Title: Patyki, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/8723. Patyki/Patyki.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        //(1)
+
+        if (a * a == (b * b) + (c * c) || b * b == (a * a) + (c * c) || c * c == (a * a) + (b * b)) System.out.println(1);
+        else if (a == b && b == c) System.out.println(2);
+        else System.out.println(0);
+    }
+}
