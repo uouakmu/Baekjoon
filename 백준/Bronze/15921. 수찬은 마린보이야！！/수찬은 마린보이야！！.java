f7e01a18906f5c48e4c183f5b01ac0993243f524
hyeogi <chan5759@naver.com>
2026-01-04 11:52:13 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if(N == 0) {
+			System.out.println("divide by zero");
+		}else {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+		
+			for(int i = 0; i < N; i++) {
+				int M = Integer.parseInt(st.nextToken());
+			}
+			System.out.println("1.00");
+		}
+	}
+}
