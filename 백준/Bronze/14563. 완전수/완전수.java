8eba0469faf2023ae340d8d616e6cfaf0e40993f
Lee Chan Hyuk <chan5759@naver.com>
2026-02-25 11:19:12 +0900

    [Bronze II] Title: 완전수, Time: 248 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Bronze/14563. 완전수/완전수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+		int [] numArr = Arrays.asList(scan.nextLine().split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+		for(int num : numArr) {
+			int result = solution(num);
+			if(num > result)
+				System.out.println("Deficient");
+			else if(num == result)
+				System.out.println("Perfect");
+			else
+				System.out.println("Abundant");
+		}
+	}
+	
+	public static int solution(int num) {
+		int sum = 0;
+		for(int i=1; i<num; i++) {
+			if(num%i == 0)
+				sum += i;
+		}
+		return sum;
+	}
+}
